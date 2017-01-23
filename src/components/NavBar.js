e1cc83017b5c4f3ad32bdf598d229c34a0ee182f
Gauthier Poulet <gauthier.poulet@gmail.com>
2017-01-23 16:58:33 +0100

    improve design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,7 @@ export default class NavBar extends React.Component {
                 <li>
                   <div className="userView">
                     <div className="background"></div>
-                    <a href="#!user"><img className="circle" alt="Gauthier POULET" src="images/gpoulet.png"/></a>
+                    <a href="#!user"><img className="circle" alt="Gauthier POULET" src="images/gp.jpg"/></a>
                     <a href="#!name"><span className="white-text name">Gauthier POULET</span></a>
                     <a href="#!email"><span className="grey-text text-lighten-3 email">Geek Software Engineer</span></a>
                   </div>
